bd4e6309a339003c0b6a2b0519236d89171c6a51
Josh Johanning <joshjohanning@github.com>
2026-01-21 15:45:41 -0600

    feat(teams): add repository and ruleset resources for GitHub organization

---
+++ teams.tf
@@ -0,0 +1,51 @@
+variable "repositories" {
+  description = "List of repositories to create in the GitHub organization"
+  type = list(object({
+    name       = string,
+    visibility = string
+  }))
+  default = []
+}
+
+resource "github_repository" "repository" {
+  provider = github.joshjohanning-org
+
+  for_each = { for repository in var.repositories : repository.name => repository }
+
+  name                 = each.value.name
+  visibility           = each.value.visibility
+  vulnerability_alerts = true
+}
+
+resource "github_repository_ruleset" "example" {
+  provider = github.joshjohanning-org
+
+  for_each = github_repository.repository
+
+  name        = "example"
+  repository  = each.value.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  # see docs https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository_ruleset
+  bypass_actors {
+    actor_id    = 1
+    actor_type  = "OrganizationAdmin"
+    bypass_mode = "always"
+  }
+
+  rules {
+    creation                = true
+    update                  = true
+    deletion                = true
+    required_linear_history = true
+    required_signatures     = true
+  }
+}
